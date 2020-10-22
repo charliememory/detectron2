5bf8bb7fda1257e6fdecca2d12755a96d9e2030d
charliememory <maliqianmemory@gmail.com>
2020-10-22 14:27:11 +0200

    change to 1meanIUVLoss

--- projects/DensePoseV2/densepose/modeling/losses/chart_global_IUV_seperated_S.py
+++ projects/DensePoseV2/densepose/modeling/losses/chart_global_IUV_seperated_S.py
@@ -334,9 +334,13 @@ class DensePoseChartGlobalIUVSeparatedSLoss(DensePoseChartLoss):
         # v_est = interpolator.extract_at_points(
         #     densepose_predictor_outputs.v[tensors_helper.index_with_dp]
         # )[j_valid_fg]
+        # return {
+        #     "loss_densepose_U": F.smooth_l1_loss(u_est, u_gt, reduction="sum") * self.w_points,
+        #     "loss_densepose_V": F.smooth_l1_loss(v_est, v_gt, reduction="sum") * self.w_points,
+        # }
         return {
-            "loss_densepose_U": F.smooth_l1_loss(u_est, u_gt, reduction="sum") * self.w_points,
-            "loss_densepose_V": F.smooth_l1_loss(v_est, v_gt, reduction="sum") * self.w_points,
+            "loss_densepose_U": F.smooth_l1_loss(u_est, u_gt, reduction="mean") * self.w_points,
+            "loss_densepose_V": F.smooth_l1_loss(v_est, v_gt, reduction="mean") * self.w_points,
         }
 
     def produce_densepose_losses_segm(
